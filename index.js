496223e0a4d8848b02f1431a7c8f3b681edba059
Simon Duc <ducsimon@gmail.com>
2022-04-19 16:48:07 +0200

    finalized the workshop

--- index.js
+++ index.js
@@ -2,18 +2,50 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-const movies = [
-  {
-    id: 0,
-    name: "Reservoir Dogs",
-  },
+const pokemons = [
   {
     id: 1,
-    name: "Star Wars",
+    name: "Karaclée",
   },
   {
     id: 2,
-    name: "Interstellar",
+    name: "Aquali",
+  },
+  {
+    id: 3,
+    name: "Raichu",
+  },
+  {
+    id: 4,
+    name: "Smogogo",
+  },
+  {
+    id: 5,
+    name: "Mr. Mime",
+  },
+  {
+    id: 6,
+    name: "Fantominus",
+  },
+  {
+    id: 7,
+    name: "Dracofeu",
+  },
+  {
+    id: 8,
+    name: "Tortank",
+  },
+  {
+    id: 9,
+    name: "Bulbizarre",
+  },
+  {
+    id: 10,
+    name: "Salamèche",
+  },
+  {
+    id: 11,
+    name: "Carapuce",
   },
 ];
 
@@ -21,13 +53,23 @@ app.get("/", (req, res) => {
   res.send('Hello World');
 });
 
-app.get("/movies", (req, res) => {
-  res.status(200).json(movies);
+app.get("/pokemons", (req, res) => {
+  const limit = parseInt(req.query.limit ?? 10);
+  const firstCharacter = req.query.firstCharacter;
+
+  let filteredPokemons = pokemons;
+
+  if(firstCharacter != null) {
+    filteredPokemons = filteredPokemons.filter(pokemon => pokemon.name.startsWith(firstCharacter));
+  }
+  res.status(200).json(filteredPokemons.filter((pokemon, index) => index < limit));
 });
 
-app.get("/movies/:id", (req, res) => {
-  if(movies.find(movie => movie.id === parseInt(req.params.id, 10))) {
-    res.status(200).send(movies[parseInt(req.params.id, 10)].name);
+app.get("/pokemons/:id", (req, res) => {
+  const {id} = req.params;
+  const pokemon = pokemons.find(pokemon => pokemon.id === parseInt(id, 10));
+  if(pokemon != null) {
+    res.status(200).send(pokemon.name);
   }
   else {
     res.sendStatus(404);
